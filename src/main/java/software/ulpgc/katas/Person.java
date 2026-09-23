d0bee1877f1a152f8158b3e43f3fac8bf0a590cd
Pablo Campos <pablo2005campos@gmail.com>
2026-09-23 09:31:11 +0100

    Person with age atribute

--- src/main/java/software/ulpgc/katas/Person.java
+++ src/main/java/software/ulpgc/katas/Person.java
@@ -6,4 +6,15 @@ import java.time.LocalDate;
  * @param name campo
  */
 public record Person(String name, LocalDate birthday) {
+
+
+
+    public int age(){
+        return toYears(LocalDate.now().toEpochDay() - birthday.toEpochDay());
+    }
+
+    public static final double DAYS_PER_YEAR = 365.25;
+    private int toYears(long days) {
+        return (int) (days/ DAYS_PER_YEAR);
+    }
 }
