2977ec09452dce52c6db5decf5e7db3bb89769a9
Pablo Campos <pablo2005campos@gmail.com>
2026-09-23 09:21:13 +0100

    Person as record

--- src/main/java/software/ulpgc/katas/Person.java
+++ src/main/java/software/ulpgc/katas/Person.java
@@ -1,13 +1,7 @@
 package software.ulpgc.katas;
 
-public class Person {
-    private final String name;  //campo
-
-    public Person(String name) {
-        this.name = name;
-    }
-
-    public String name() {
-        return name;
-    }
+/**
+ * @param name campo
+ */
+public record Person(String name) {
 }
