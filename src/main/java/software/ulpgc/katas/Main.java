8187bcbf8b90c44e43a305f100b23eb7e2714888
Pablo Campos <pablo2005campos@gmail.com>
2026-09-23 09:36:34 +0100

    main updated

--- src/main/java/software/ulpgc/katas/Main.java
+++ src/main/java/software/ulpgc/katas/Main.java
@@ -3,6 +3,7 @@ package software.ulpgc.katas;
 import java.time.LocalDate;
 
 public class Main {
+
     static void main() {
         Person lucia = new Person("Lucia", LocalDate.of(2005, 5, 12));
         System.out.println(lucia.age());
